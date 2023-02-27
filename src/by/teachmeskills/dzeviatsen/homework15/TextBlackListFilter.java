545c6d70e357697993322d24e7ddc5c464c225c1
DaniilDzeviatsen <deviat.daan@gmail.com>
2023-02-27 14:56:47 +0300

    final

---
+++ src/by/teachmeskills/dzeviatsen/homework15/TextBlackListFilter.java
@@ -0,0 +1,54 @@
+package by.teachmeskills.dzeviatsen.homework15;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TextBlackListFilter {
+    private String[] badWords;
+
+    public TextBlackListFilter(String badWordss) {
+        this.badWords = badWordss.split(", ");
+    }
+
+    public boolean ifContainsBadWords(String userText) {
+        for (int i = 0; i < badWords.length; i++) {
+            Pattern pattern = Pattern.compile("\\b" + Pattern.quote(badWords[i]) + "\\b",
+                    Pattern.UNICODE_CHARACTER_CLASS | Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
+            Matcher matcher = pattern.matcher(userText);
+            if (matcher.find()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public int badWordsCount(String userText) {
+        int count = 0;
+        for (int i = 0; i < badWords.length; i++) {
+            Pattern pattern = Pattern.compile("\\b" + Pattern.quote(badWords[i]) + "\\b",
+                    Pattern.UNICODE_CHARACTER_CLASS | Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
+            Matcher matcher = pattern.matcher(userText);
+            if (matcher.find()) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public String toSencoredText(String userText) {
+        for (int i = 0; i < badWords.length; i++) {
+
+            Pattern pattern = Pattern.compile("\\b" + Pattern.quote(badWords[i]) + "\\b",
+                    Pattern.UNICODE_CHARACTER_CLASS | Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
+            Matcher matcher = pattern.matcher(userText);
+            if (matcher.find()) {
+                if (userText.contains((badWords[i]))) {
+                    userText = userText.replace(badWords[i], "####");
+                }
+            }
+
+        }return userText;
+    }
+}
